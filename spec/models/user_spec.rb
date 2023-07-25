5b0510e99c2becc378b8fd8276a0b0aa1b5939bd
mathieu jelsch <mathieu.jelsch@gmail.com>
2023-07-25 14:28:48 +0200

    tout est fini

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe User, type: :model do
 
       user = User.new
 
-      allow(user).to receive(:callApi).and_return('[{"name": "John doe"}, {}, {}]') # permet au test de recevoir d'autre donné
+      allow(user).to receive(:callApi).and_return('[{"name": "John doe"}, {}, {}]') # autorise au test de recevoir d'autre donné
 
-      expect(user.import).to eq("John doe")
+      expect(user.import).to eq("John doe") # la methode import a été codé dans le model user, elle va retourner le premier nom de l'api
 
     end
   end
